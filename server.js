92ed86f9b847d1af77d5810ca4488cf5509021b2
DIVIN <divinmathems58@gmail.com>
2025-06-01 23:54:00 +0200

    homepage

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/testing', (req, res) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'homePage', 'homep.html'));
+  res.sendFile(path.join(__dirname, 'homePage', 'testing.html'));
 });
 
 app.listen(PORT, () => {
